b07d693710cad9866dace5fcfb7854cb4cd12939
WChen123123123 <wc051905@163.com>
2022-07-08 14:32:26 +0800

    first commit

---
+++ chen-admin/src/main/java/com/chen/ChenServletInitializer.java
@@ -0,0 +1,18 @@
+package com.chen;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中进行部署
+ * 
+ * @author ruoyi
+ */
+public class ChenServletInitializer extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(ChenApplication.class);
+    }
+}
